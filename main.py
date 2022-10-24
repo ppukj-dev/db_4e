92dab4b6139fd076246f91f12314e460878b5cbb
Zixus <gaudensius.dimas@gmail.com>
2022-10-24 17:50:16 +0700

    turn the title into clickable link

--- main.py
+++ main.py
@@ -9,20 +9,22 @@ from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.all()
 
-bot = commands.Bot(command_prefix=[';;'], intents=intents)
+bot = commands.Bot(command_prefix=[";;"], intents=intents)
+
 
 def to_markdown(html: str) -> str:
     html = html.replace("<span", "\t|\t<span")
     md = markdownify.markdownify(html=html)
     return md
 
+
 async def search_data(ctx, search, table):
     data = get_data(table, search)
-    if len(data) == 0 :
+    if len(data) == 0:
         await ctx.send("Not found.")
         return
     elif len(data) == 1:
@@ -30,8 +32,9 @@ async def search_data(ctx, search, table):
         description = to_markdown(data[0][2])
         if len(description) > 2000:
             description = description[:2000] + "... (too long)"
-        description += "\n\nFull: [*IWS Link*](http://iws.mx/dnd/?view={})".format(data[0][3])
-        embed = discord.Embed(title=title, description=description)
+        url = "http://iws.mx/dnd/?view={}".format(data[0][3])
+        description += "\n\nFull: [*IWS Link*]({url})"
+        embed = discord.Embed(title=title, description=description, url=url)
         await ctx.send(embed=embed)
         return
     else:
@@ -43,14 +46,20 @@ async def search_data(ctx, search, table):
             map[map_key] = x
             list = list + "`{0}. {1}`\n".format(idx, x[1])
             idx += 1
+
         def followup(message):
-            return (message.content.isnumeric() or message.content == "c") and message.author == ctx.message.author
+            return (
+                message.content.isnumeric() or message.content == "c"
+            ) and message.author == ctx.message.author
+
         description = """Do you mean?\n{}""".format(list)
         embed = discord.Embed(title="Multiple Found", description=description)
         embed.set_footer(text="Type 1-10 to choose, or c to cancel.")
         option_message = await ctx.send(embed=embed)
         try:
-            followup_message = await bot.wait_for("message",timeout=60.0 ,check=followup)
+            followup_message = await bot.wait_for(
+                "message", timeout=60.0, check=followup
+            )
         except asyncio.TimeoutError:
             await option_message.delete()
             await ctx.send("Time Out")
@@ -64,92 +73,114 @@ async def search_data(ctx, search, table):
             description = to_markdown(data[2])
             if len(description) > 2000:
                 description = description[:2000] + "... (too long)"
-            description += "\n\nFull: [*IWS Link*](http://iws.mx/dnd/?view={})".format(data[3])
+            description += "\n\nFull: [*IWS Link*](http://iws.mx/dnd/?view={})".format(
+                data[3]
+            )
             embed = discord.Embed(title=title, description=description)
             await option_message.delete()
             await followup_message.delete()
             await ctx.send(embed=embed)
             return
-        
 
-@bot.command(name='power')
+
+@bot.command(name="power")
 async def power_search(ctx, *, search):
     await search_data(ctx, search, "power")
 
-@bot.command(name='item')
+
+@bot.command(name="item")
 async def item_search(ctx, *, search):
     await search_data(ctx, search, "item")
 
-@bot.command(name='feat')
+
+@bot.command(name="feat")
 async def feat_search(ctx, *, search):
     await search_data(ctx, search, "feat")
 
-@bot.command(name='poison')
+
+@bot.command(name="poison")
 async def poison_search(ctx, *, search):
     await search_data(ctx, search, "poison")
 
-@bot.command(name='ritual')
+
+@bot.command(name="ritual")
 async def ritual_search(ctx, *, search):
     await search_data(ctx, search, "ritual")
 
-@bot.command(name='weapon')
+
+@bot.command(name="weapon")
 async def weapon_search(ctx, *, search):
     await search_data(ctx, search, "weapon")
 
-@bot.command(name='theme')
+
+@bot.command(name="theme")
 async def theme_search(ctx, *, search):
     await search_data(ctx, search, "theme")
 
-@bot.command(name='disease')
+
+@bot.command(name="disease")
 async def disease_search(ctx, *, search):
     await search_data(ctx, search, "disease")
 
-@bot.command(name='glossary')
+
+@bot.command(name="glossary")
 async def glossary_search(ctx, *, search):
     await search_data(ctx, search, "glossary")
 
-@bot.command(name='implement')
+
+@bot.command(name="implement")
 async def implement_search(ctx, *, search):
     await search_data(ctx, search, "implement")
 
-@bot.command(name='armor')
+
+@bot.command(name="armor")
 async def armor_search(ctx, *, search):
     await search_data(ctx, search, "armor")
 
-@bot.command(name='companion')
+
+@bot.command(name="companion")
 async def companion_search(ctx, *, search):
     await search_data(ctx, search, "companion")
 
-@bot.command(name='trap')
+
+@bot.command(name="trap")
 async def trap_search(ctx, *, search):
     await search_data(ctx, search, "trap")
 
-@bot.command(name='deity')
+
+@bot.command(name="deity")
 async def deity_search(ctx, *, search):
     await search_data(ctx, search, "deity")
 
-@bot.command(name='background')
+
+@bot.command(name="background")
 async def background_search(ctx, *, search):
     await search_data(ctx, search, "background")
 
-@bot.command(name='epicdestiny')
+
+@bot.command(name="epicdestiny")
 async def epicdestiny_search(ctx, *, search):
     await search_data(ctx, search, "epicdestiny")
 
-@bot.command(name='class')
+
+@bot.command(name="class")
 async def class_search(ctx, *, search):
     await search_data(ctx, search, "class")
 
-@bot.command(name='monster')
+
+@bot.command(name="monster")
 async def monster_search(ctx, *, search):
     await search_data(ctx, search, "monster")
 
-@bot.command(name='paragonpath')
+
+@bot.command(name="paragonpath")
 async def paragonpath_search(ctx, *, search):
     await search_data(ctx, search, "paragonpath")
 
-@bot.command(name='race')
+
+@bot.command(name="race")
 async def race_search(ctx, *, search):
     await search_data(ctx, search, "race")
+
 
 bot.run(TOKEN)
